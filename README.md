6c505de8bd3b4a833081825c6654591a81b64096
kevinhooke <kevin.hooke@gmail.com>
2022-06-16 21:58:44 -0700

    added read from first file

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # phrase-generator
 Generates phrases from random words read from files of nouns and verbs
 
-As source files this uses dictionary files from Princeton Univercity's WordNet project, which can be downloaded fom here:
+As source files this uses dictionary files from Princeton University's WordNet project, which can be downloaded from here:
 https://wordnet.princeton.edu/download although other files of source words will work too.
 
-If using a download of WordNet, unzip it to this same folder. A script parse-source-files.sh uses sed to extract just the words from the dictionary files.
+If using a download of WordNet, unzip it to this same folder. A script parse-source-files.sh uses sed to extract just the words from the dictionary files into files read by this app to generate the phrases.
--- src/main/java/kh/phrasegenerator/PhraseGenerator.java
+++ src/main/java/kh/phrasegenerator/PhraseGenerator.java
@@ -1,15 +1,36 @@
 package kh.phrasegenerator;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class PhraseGenerator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		PhraseGenerator generator = new PhraseGenerator();
+		
+		String word1 = generator.getRandomWordFromFile("./noun.person.parsed.txt");
+		
+		System.out.println(word1);
 
 	}
 
-	String getRandomWordFromFile(String filename) {
+	String getRandomWordFromFile(String filename) throws Exception{
 		String word = null;
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		List<String> words = new ArrayList<>();
+		String nextWord = null;
+		while((nextWord = reader.readLine()) != null) {
+			words.add(nextWord);
+		}
 		
+		int wordsInFile = words.size();
+		int randomWord = (new Random()).nextInt(wordsInFile);
+		word = words.get(randomWord);
+		
+		reader.close();
 		return word;
 	}
 }
