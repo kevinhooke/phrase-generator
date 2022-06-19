89a6d538261e1cfb304bfccc63d0f7d899291353
kevinhooke <kevin.hooke@gmail.com>
2022-06-19 13:57:35 -0700

    Added Files example

--- src/main/java/kh/phrasegenerator/PhraseGenerator.java
+++ src/main/java/kh/phrasegenerator/PhraseGenerator.java
@@ -2,9 +2,12 @@ package kh.phrasegenerator;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class PhraseGenerator {
 
@@ -13,8 +16,8 @@ public class PhraseGenerator {
 		
 		String personNoun = generator.getRandomWordFromFile("./noun.person.parsed.txt");
 		String verb = generator.getRandomWordFromFile("./verb.change.parsed.txt");
-		String noun2 = generator.getRandomWordFromFile("./noun.person.parsed.txt");
-		String adjective = generator.getRandomWordFromFile("./adj.all.parsed.txt");
+		String noun2 = generator.getRandomWordFromFileUsingFilesApi("./noun.person.parsed.txt");
+		String adjective = generator.getRandomWordFromFileUsingFilesApi("./adj.all.parsed.txt");
 		String pp = generator.formPresentParticiple(verb);
 		
 		String articlePlusAdjective = generator.addArticle(adjective);
@@ -36,6 +39,7 @@ public class PhraseGenerator {
 		if(noun.toLowerCase().startsWith("a") 
 			|| noun.toLowerCase().startsWith("e")
 			|| noun.toLowerCase().startsWith("i")
+			|| noun.toLowerCase().startsWith("o")
 			|| noun.toLowerCase().startsWith("u")){
 			result = "an " + noun;
 		}
@@ -45,6 +49,17 @@ public class PhraseGenerator {
 		return result;
 	}
 
+	/**
+	 * Retrieves random word from a file by loading each line from the file, and then
+	 * returning a random String from the List<String> read from the file.
+	 * 
+	 * Note this is only efficient for small files since the intire file content is
+	 * read into memory.
+	 * 
+	 * @param filename
+	 * @return
+	 * @throws Exception
+	 */
 	String getRandomWordFromFile(String filename) throws Exception{
 		String word = null;
 		BufferedReader reader = new BufferedReader(new FileReader(filename));
@@ -61,6 +76,26 @@ public class PhraseGenerator {
 		word = word.replaceAll("_", " ");
 		
 		reader.close();
+		return word;
+	}
+	
+	/**
+	 * Retrieves random word from a file using Java 8 Files.lines().
+	 * 
+	 * @param filename
+	 * @return
+	 * @throws Exception
+	 */
+	String getRandomWordFromFileUsingFilesApi(String filename) throws Exception{
+		String word = null;
+		try (Stream<String> lines = Files.lines(Paths.get(filename))) {
+			long wordsInFile = Files.lines(Paths.get(filename)).count();
+	
+			long randomWord = (new Random()).nextLong(wordsInFile);
+			word = lines.skip(randomWord).findFirst().get();
+		}		
+		
+		word = word.replaceAll("_", " ");
 		return word;
 	}
 	
